8454ba7763adac2df875caee457cd75fbf44e91d
Dhananjay <108760208+Dhananjay-M-Shinde@users.noreply.github.com>
2024-04-25 10:23:03 +0000

    completed.

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors())
+
+app.use(express.json({limit:"50mb"}))
+app.use(express.urlencoded({extended: true, limit: "50mb"}))
+// app.use(express.static("public"))
+app.use("/upload", express.static('public/temp'))
+app.use(cookieParser())
+
+
+// routes import
+
+import userRouter from "./routes/user.routes.js"
+import expenseRouter from "./routes/expense.routes.js"
+
+
+// routes declaration
+// http://localhost:8000/api/v1/users/register
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/expenses", expenseRouter);
+
+
+export {app}
